9ac097ab003b7ee4c1bb3afa9f626224f80d846a
AraVraelHalt <ma8540@rit.edu>
2026-03-26 00:57:05 -0400

    Initialized Go module

---
+++ cmd/server/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("API Contract Break Detector backend running")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AraVraelHalt/API-Contract-Detector
+
+go 1.26.1
